59eca186f054ac1f36a5b2abc4cd7ed1a22dce17
Ron Welch <mmcelt@yahoo.com>
2020-12-24 15:06:19 -0700

    18 - Player Name Fixer

--- Assets/Scripts/SetupLocalPlayer.cs
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -6,112 +6,78 @@ using UnityEngine.UI;
 
 public class SetupLocalPlayer : NetworkBehaviour
 {
-	#region Fields
+	public Text namePrefab;
+	public Text nameLabel;
+	public Transform namePos;
+	string textboxname = "";
+	string colourboxname = "";
 
-	public Text _namePrefab;
-	public Text _nameText;
-	public Transform _namePos;
+	[SyncVar(hook = "OnChangeName")]
+	public string pName = "player";
 
-	string _textboxName;
-	string _colorboxName;
+	[SyncVar(hook = "OnChangeColour")]
+	public string pColour = "#ffffff";
 
-	[SyncVar(hook = nameof(OnChangeName))]
-	public string _pName = "";
+	#region Client Methods
 
-	[SyncVar(hook = nameof(OnChangeColor))]
-	public string _pColor = "";
-
-	#endregion
-
-	#region MonoBehaviour Methods
-
-	void OnGUI()
+	void OnChangeName(string n)
 	{
-		if (isLocalPlayer)
-		{
-			_textboxName = GUI.TextField(new Rect(25, 15, 100, 25), _textboxName);
-			
-			if(GUI.Button(new Rect(130, 15, 35, 25), "Set"))
-			{
-				CmdChangeName(_textboxName);
-			}
-
-			_colorboxName = GUI.TextField(new Rect(170, 15, 100, 25), _colorboxName);
-
-			if (GUI.Button(new Rect(275, 15, 35, 25), "Set"))
-				CmdChangeColor(_colorboxName);
-		}
+		pName = n;
+		nameLabel.text = pName;
 	}
 
-	void Start() 
+	void OnChangeColour(string n)
 	{
-		if (isLocalPlayer)
-		{
-			GetComponent<PlayerController>().enabled = true;
-			CameraFollow360.player = gameObject.transform;
-		}
-		else
-		{
-			GetComponent<PlayerController>().enabled = false;
-		}
+		pColour = n;
+		Renderer[] rends = GetComponentsInChildren<Renderer>();
 
-		MakeThePlayerNameLabel();
-	}
-
-	void Update()
-	{
-		Vector3 namePos = Camera.main.WorldToScreenPoint(_namePos.position);
-		_nameText.transform.position = namePos;
+		foreach (Renderer r in rends)
+		{
+			if (r.gameObject.name == "BODY")
+				r.material.SetColor("_Color", ColorFromHex(pColour));
+		}
 	}
 	#endregion
 
 	#region Server Methods
 
 	[Command]
-	void CmdChangeName(string newName)
+	public void CmdChangeName(string newName)
 	{
-		_pName = newName;
-		_nameText.text = _pName;
+		pName = newName;
+		nameLabel.text = pName;
 	}
 
 	[Command]
-	void CmdChangeColor(string newColor)
+	public void CmdChangeColour(string newColour)
 	{
-		_pColor = newColor;
-		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		pColour = newColour;
+		Renderer[] rends = GetComponentsInChildren<Renderer>();
 
-		foreach (Renderer renderer in renderers)
+		foreach (Renderer r in rends)
 		{
-			if (renderer.gameObject.name == "BODY")
-				renderer.material.SetColor("_Color", ColorFromHex(_pColor));
+			if (r.gameObject.name == "BODY")
+				r.material.SetColor("_Color", ColorFromHex(pColour));
 		}
 	}
 	#endregion
 
-	#region Client Methods
+	#region Unity Methods
 
-	void MakeThePlayerNameLabel()
+	void OnGUI()
 	{
-		GameObject canvas = GameObject.FindGameObjectWithTag("MainCanvas");
-		_nameText = Instantiate(_namePrefab, canvas.transform);
-	}
-
-	void OnChangeName(string newName)
-	{
-		_nameText.text = newName;
-	}
-
-	void OnChangeColor(string newColor)
-	{
-		_pColor = newColor;
-		Renderer[] renderers = GetComponentsInChildren<Renderer>();
-
-		foreach(Renderer renderer in renderers)
+		if (isLocalPlayer)
 		{
-			if (renderer.gameObject.name == "BODY")
-				renderer.material.SetColor("_Color", ColorFromHex(_pColor));
+			textboxname = GUI.TextField(new Rect(25, 15, 100, 25), textboxname);
+			if (GUI.Button(new Rect(130, 15, 35, 25), "Set"))
+				CmdChangeName(textboxname);
+
+			colourboxname = GUI.TextField(new Rect(170, 15, 100, 25), colourboxname);
+			if (GUI.Button(new Rect(275, 15, 35, 25), "Set"))
+				CmdChangeColour(colourboxname);
 		}
 	}
+
 
 	//Credit for this method: from http://answers.unity3d.com/questions/812240/convert-hex-int-to-colorcolor32.html
 	//hex for testing green: 04BF3404  red: 9F121204  blue: 221E9004
@@ -128,6 +94,48 @@ public class SetupLocalPlayer : NetworkBehaviour
 			a = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
 		}
 		return new Color32(r, g, b, a);
+	}
+
+	void Start()
+	{
+		if (isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+			CameraFollow360.player = this.gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+
+		GameObject canvas = GameObject.FindWithTag("MainCanvas");
+		nameLabel = Instantiate(namePrefab, Vector3.zero, Quaternion.identity) as Text;
+		nameLabel.transform.SetParent(canvas.transform);
+	}
+
+	public void OnDestroy()
+	{
+		if (nameLabel != null)
+			Destroy(nameLabel.gameObject);
+	}
+
+	void Update()
+	{
+		//determine if the object is inside the camera's viewing volume
+		if (nameLabel != null)
+		{
+			Vector3 screenPoint = Camera.main.WorldToViewportPoint(this.transform.position);
+			bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 &&
+								 screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+			//if it is on screen draw its label attached to is name position
+			if (onScreen)
+			{
+				Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(namePos.position);
+				nameLabel.transform.position = nameLabelPos;
+			}
+			else //otherwise draw it WAY off the screen 
+				nameLabel.transform.position = new Vector3(-1000, -1000, 0);
+		}
 	}
 	#endregion
 }
