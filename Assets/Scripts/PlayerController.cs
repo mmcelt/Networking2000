416bd6a12f03521f234b5a64f349dc3a449c5ee8
Ron Welch <mmcelt@yahoo.com>
2020-12-24 10:34:59 -0700

    Redo - S4 - 15: Adding Names to Player Pt1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿//adapted from example script available at
+//https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	float _translation, _rotation;
+
+	Rigidbody _theRB;
+
+	void Start()
+	{
+		_theRB = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		_translation = Input.GetAxis("Vertical") * speed;
+		_rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		_translation *= Time.deltaTime;
+		_rotation *= Time.deltaTime;
+
+		//THIS IS FOR SYNCING THE TRANSFORM...
+		//transform.Translate(0, 0, translation);
+		//transform.Rotate(0, rotation, 0);
+
+		//THIS IS FOR SYNCING THE RIGIBODY...
+		Quaternion turn = Quaternion.Euler(0f, _rotation, 0f);
+		_theRB.MovePosition(_theRB.position + transform.forward * _translation);
+		_theRB.MoveRotation(_theRB.rotation * turn);
+	}
+}
---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+	#region Fields
+
+	public Text _namePrefab;
+	public Text _nameText;
+	public Transform _namePos;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		if (isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+			CameraFollow360.player = gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+
+		}
+
+		MakeThePlayerNameLabel();
+	}
+
+	void Update()
+	{
+		Vector3 namePos = Camera.main.WorldToScreenPoint(_namePos.position);
+		_nameText.transform.position = namePos;
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+	void MakeThePlayerNameLabel()
+	{
+		GameObject canvas = GameObject.FindGameObjectWithTag("MainCanvas");
+		_nameText = Instantiate(_namePrefab,canvas.transform);
+	}
+	#endregion
+}
