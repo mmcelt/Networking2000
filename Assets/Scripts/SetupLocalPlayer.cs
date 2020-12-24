cf49868c9ebca7efd7c3449bac6d789df258555d
Ron Welch <mmcelt@yahoo.com>
2020-12-24 14:26:02 -0700

    17 - Customizing Player Color

--- Assets/Scripts/SetupLocalPlayer.cs
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -12,10 +12,14 @@ public class SetupLocalPlayer : NetworkBehaviour
 	public Text _nameText;
 	public Transform _namePos;
 
-	string _textboxName = "";
+	string _textboxName;
+	string _colorboxName;
 
 	[SyncVar(hook = nameof(OnChangeName))]
-	public string pName = "";
+	public string _pName = "";
+
+	[SyncVar(hook = nameof(OnChangeColor))]
+	public string _pColor = "";
 
 	#endregion
 
@@ -31,6 +35,11 @@ public class SetupLocalPlayer : NetworkBehaviour
 			{
 				CmdChangeName(_textboxName);
 			}
+
+			_colorboxName = GUI.TextField(new Rect(170, 15, 100, 25), _colorboxName);
+
+			if (GUI.Button(new Rect(275, 15, 35, 25), "Set"))
+				CmdChangeColor(_colorboxName);
 		}
 	}
 
@@ -44,7 +53,6 @@ public class SetupLocalPlayer : NetworkBehaviour
 		else
 		{
 			GetComponent<PlayerController>().enabled = false;
-
 		}
 
 		MakeThePlayerNameLabel();
@@ -62,8 +70,21 @@ public class SetupLocalPlayer : NetworkBehaviour
 	[Command]
 	void CmdChangeName(string newName)
 	{
-		pName = newName;
-		_nameText.text = pName;
+		_pName = newName;
+		_nameText.text = _pName;
+	}
+
+	[Command]
+	void CmdChangeColor(string newColor)
+	{
+		_pColor = newColor;
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+		foreach (Renderer renderer in renderers)
+		{
+			if (renderer.gameObject.name == "BODY")
+				renderer.material.SetColor("_Color", ColorFromHex(_pColor));
+		}
 	}
 	#endregion
 
@@ -78,6 +99,35 @@ public class SetupLocalPlayer : NetworkBehaviour
 	void OnChangeName(string newName)
 	{
 		_nameText.text = newName;
+	}
+
+	void OnChangeColor(string newColor)
+	{
+		_pColor = newColor;
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+		foreach(Renderer renderer in renderers)
+		{
+			if (renderer.gameObject.name == "BODY")
+				renderer.material.SetColor("_Color", ColorFromHex(_pColor));
+		}
+	}
+
+	//Credit for this method: from http://answers.unity3d.com/questions/812240/convert-hex-int-to-colorcolor32.html
+	//hex for testing green: 04BF3404  red: 9F121204  blue: 221E9004
+	Color ColorFromHex(string hex)
+	{
+		hex = hex.Replace("0x", "");
+		hex = hex.Replace("#", "");
+		byte a = 255;
+		byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+		byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+		byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+		if (hex.Length == 8)
+		{
+			a = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+		}
+		return new Color32(r, g, b, a);
 	}
 	#endregion
 }
