5e3d2fbad630b6e491439a277c0ef9f60b297859
Ron Welch <mmcelt@yahoo.com>
2020-12-26 11:46:31 -0700

    SECTION 6: HUMANOID ANIMATIONS: 28 - Syncing Humanoid Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour 
+public class PlayerController : MonoBehaviour
 {
-	Rigidbody rb;
-	 float speed = 25.0F;
-	 float rotationSpeed = 50.0F;
+	Rigidbody _rb;
+	Animator _anim;
 
-	 void Start()
+	[SerializeField] float _speed = 25.0F;
+	[SerializeField] float _rotationSpeed = 50.0F;
+
+	void Start()
 	{
-		  rb = GetComponent<Rigidbody>();
-	 }
-	
-	 // Update is called once per frame
-	void FixedUpdate () 
+		_rb = GetComponent<Rigidbody>();
+		_anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate()
 	{
-		  float translation = Input.GetAxis("Vertical") * speed;
-		  float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-		  translation *= Time.deltaTime;
-		  rotation *= Time.deltaTime;
-		  Quaternion turn = Quaternion.Euler(0f,rotation,0f);
-		  rb.MovePosition(rb.position + transform.forward * translation);
-		  rb.MoveRotation(rb.rotation * turn);
+		float translation = Input.GetAxis("Vertical") * _speed;
+		float rotation = Input.GetAxis("Horizontal") * _rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		Quaternion turn = Quaternion.Euler(0f, rotation, 0f);
+		_rb.MovePosition(_rb.position + transform.forward * translation);
+		_rb.MoveRotation(_rb.rotation * turn);
+
+		//THIS CODE IS FOR THE ANIMATION PART OF THE COURSE ONLY!
+		if (translation != 0)
+		{
+			_anim.SetBool("Idling", false);
+			GetComponent<SetupLocalHumanPlayer>().CmdChangeAnimState("run");
+		}
+		else
+		{
+			_anim.SetBool("Idling", true);
+			GetComponent<SetupLocalHumanPlayer>().CmdChangeAnimState("idle");
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_anim.SetTrigger("Attacking");
+			GetComponent<SetupLocalHumanPlayer>().CmdChangeAnimState("attack");
+		}
 	}
 }
---
+++ Assets/Scripts/SetupLocalHumanPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalHumanPlayer: NetworkBehaviour
+{
+	#region Fields
+
+	Animator _anim;
+
+	[SyncVar(hook = nameof(OnChangeAnimation))]
+	public string _animState;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		_anim = GetComponent<Animator>();
+		_anim.SetBool("Idling", true);
+
+		if (isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+			CameraFollow360.player = gameObject.transform;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+	}
+	#endregion
+
+	#region Client Methods
+
+	void OnChangeAnimation(string newState)
+	{
+		if (isLocalPlayer) return;
+
+		UpdateAnimationState(newState);
+	}
+	#endregion
+
+	#region Server Methods
+
+	[Command]
+	public void CmdChangeAnimState(string newState)
+	{
+		UpdateAnimationState(newState);
+	}
+	#endregion
+
+	#region Common Private Methods
+	void UpdateAnimationState(string newState)
+	{
+		if (_animState == newState) return;
+
+		_animState = newState;
+
+		if (_animState == "idle")
+			_anim.SetBool("Idling", true);
+		else if (_animState == "run")
+			_anim.SetBool("Idling", false);
+		else if (_animState == "attack")
+			_anim.SetTrigger("Attacking");
+	}
+	#endregion
+}
