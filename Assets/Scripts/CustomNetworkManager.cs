50d5e52543fd4938e93c2e73bce95ffaad4d3318
Ron Welch <mmcelt@yahoo.com>
2020-12-26 13:34:21 -0700

    SECTION 7: CUSTOMIZING THE NETWORK MANAGER: 29 - Picking a Custom Player

---
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MsgTypes
+{
+	public const short PlayerPrefabSelect = MsgType.Highest + 1;
+
+	public class PlayerPrefabMsg : MessageBase
+	{
+		public short controllerID;
+		public short prefabIndex;
+	}
+}
+
+public class CustomNetworkManager : NetworkManager
+{
+	#region Fields
+
+	public short playerPrefabIndex;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	#endregion
+
+	#region Server Methods
+
+	public override void OnStartServer()
+	{
+		NetworkServer.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnResponsePrefab);
+		base.OnStartServer();
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+		msg.controllerID = playerControllerId;
+		NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefabSelect, msg);
+	}
+
+	void OnResponsePrefab(NetworkMessage netMsg)
+	{
+		MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();
+		playerPrefab = spawnPrefabs[msg.prefabIndex];
+		base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
+	}
+	#endregion
+
+	#region Client Methods
+
+	public override void OnClientConnect(NetworkConnection conn)
+	{
+		client.RegisterHandler(MsgTypes.PlayerPrefabSelect, OnRequestPrefab);
+		base.OnClientConnect(conn);
+	}
+
+	void OnRequestPrefab(NetworkMessage netMsg)
+	{
+		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+		msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
+		msg.prefabIndex = playerPrefabIndex;
+		client.Send(MsgTypes.PlayerPrefabSelect, msg);
+	}
+	#endregion
+}
